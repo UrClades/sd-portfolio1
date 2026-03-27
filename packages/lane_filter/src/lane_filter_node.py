d5bf4281b089aabad833d4a57f979550a84432f8
Jesse Coenraad <jessecoenraad2004@gmail.com>
2026-03-27 14:17:13 +0100

    update

--- packages/lane_filter/src/lane_filter_node.py
+++ packages/lane_filter/src/lane_filter_node.py
@@ -117,12 +117,6 @@ class LaneFilterNode(DTROS):
         )
 
 
-
-        # Set up a timer for prediction (if we got encoder data) since that data can come very quickly
-  #      rospy.Timer(rospy.Duration(1 / self._predict_freq), self.cbPredict)
-        self.publishEstimate(self.last_update_header)
-
-
     def cbEpisodeStart(self, msg):
         rospy.loginfo("Lane Filter Resetting")
         self.filter.initialize_belief()
